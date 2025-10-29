cd2af3f4bd117668d279957ed01fb5fa5f94233b
Kozambiq <cyrussgayola1@gmail.com>
2025-10-29 20:49:13 +0800

    Enhance student data entry with input validation
    Added input validation for student data entry and improved comments.

--- main.java
+++ main.java
@@ -1,6 +1,10 @@
-import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.Scanner; // import scanner for user in
 
+// created class object Student
 class Student{
+
+    // characteristics of the student
     String name;
     int age;
     String gender;
@@ -8,6 +12,7 @@ class Student{
     String birthday;
     double gpa;
 
+    // constructor for student
     Student(String name, int age, String gender, String year, String birthday, double gpa){
         this.name = name;
         this.age = age;
@@ -17,6 +22,8 @@ class Student{
         this.gpa = gpa;
     }
 
+
+    //method for displating the students info
     void displayInfo(){
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
@@ -27,56 +34,81 @@ class Student{
     }
 }
 
+// main method
 public class main{
-public static void main(String[] args) {
+    public static void main(String[] args) {        
     
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // intialized scanner for user input
 
-    int count;
-    String name;
-    int age;
-    String gender;
-    String year;
-    String birthday;
-    double gpa;
+        // initializing variables
+        int count;
+        String name;
+        int age;
+        String gender;
+        String year;
+        String birthday;
+        double gpa;
 
-    System.out.print("How many students would you like to store?: ");
-    count = scanner.nextInt();
-    scanner.nextLine();
+        // prompt asking user how many students they would like to store
+        System.out.print("How many students would you like to store?: ");
+        count = scanner.nextInt(); 
+        scanner.nextLine(); // nextLine()
 
-    Student[] students = new Student[count];
+        Student[] students = new Student[count]; // made an array of object where it will store the students
 
-    for(int i = 0; i < count; i++){
-        System.out.print("\nStudent name: ");
-        name = scanner.nextLine();
+        // for loop that ends when user choice 
+        for(int i = 0; i < count; i++){
 
-        System.out.print("Student age: ");
-        age = scanner.nextInt();
-        scanner.nextLine();
+
+            try{
+
+                //asks for student name
+                System.out.print("\nStudent name: ");
+                name = scanner.nextLine();
+
+                // asks for student age
+                System.out.print("Student age: ");
+                age = scanner.nextInt();
+                scanner.nextLine(); // debuffer line
+                
+                // asks for student gender
+                System.out.print("Student gender: ");
+                gender = scanner.nextLine();
+
+                // asks for student year
+                System.out.print("Student year: ");
+                year = scanner.nextLine();
+
+                // asks for student birthday
+                System.out.print("Student birthday: ");
+                birthday = scanner.nextLine();
+
+                // asks for student gpa
+                System.out.print("Student gpa: ");
+                gpa = scanner.nextDouble();
+                scanner.nextLine(); // debuffer line
+
+                students[i] = new Student(name, age, gender, year, birthday, gpa); // returns the given data to the Student object
+
+            }
+
+            // when words are entered to int scanner this code block runs
+            catch (InputMismatchException e){
+                System.out.println("Invalid input! "); // prints invalid
+                scanner.nextLine(); // debuffer line
+                i--; // resets the iteration by 1
+            }
+
+        }
+
+        // formats out the all students tab
+        System.out.println("===All Students===");
+        for(Student s : students) {
+            s.displayInfo(); // calls the method that handles all the student info
+        }
         
-        System.out.print("Student gender: ");
-        gender = scanner.nextLine();
 
-        System.out.print("Student year: ");
-        year = scanner.nextLine();
+        scanner.close(); // closes the scanner
 
-        System.out.print("Student birthday: ");
-        birthday = scanner.nextLine();
-
-        System.out.print("Student gpa: ");
-        gpa = scanner.nextDouble();
-        scanner.nextLine();
-
-        students[i] = new Student(name, age, gender, year, birthday, gpa);
     }
-
-    System.out.println("===All Students===");
-    for(Student s : students) {
-        s.displayInfo();
-    }
-    
-
-    scanner.close();
-
-}
 }
